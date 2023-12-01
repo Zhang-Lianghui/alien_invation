d1785ef8dfa643bce1b7f4ce592c67086a5092b1
leon <zhang_lianghui@outlook.com>
2023-12-01 10:16:48 +0800

    重构run_game，将监视键鼠和更新屏幕独立成私有方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,15 +18,21 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+    def _check_events(self):
+        """监视键盘和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕元素并显示"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
